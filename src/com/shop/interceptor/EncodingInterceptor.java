459a30eff1b00fd6e12e612c506b07f32750889b
Allen6-a <1335048629@qq.com>
2020-06-02 20:41:06 +0800

    zhong

---
+++ src/com/shop/interceptor/EncodingInterceptor.java
@@ -0,0 +1,31 @@
+package com.shop.interceptor;
+
+import java.util.Iterator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class EncodingInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+			Object arg2) throws Exception {
+		response.setContentType("text/html;charset=UTF-8");
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response,
+			Object arg2, ModelAndView arg3) throws Exception {
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+	}
+}
